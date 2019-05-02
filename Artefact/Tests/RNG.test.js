33e829de38e6f6ffd4787ffc4407a84fab5609c8
TristanBarlow <tristanbarlowg@gmail.com>
2019-05-02 14:58:47 +0100

    Reformatted Tests Added test evidence Fixed all references

--- Artefact/Tests/RNG.test.js
+++ Artefact/Tests/RNG.test.js
@@ -3,31 +3,70 @@ let rng = require("../MainProject/lib/RNG.js");
 let seed = Math.floor(Math.random()*1000);
 let RNG_1 = new rng.RNG(seed);
 let RNG_2 =new  rng.RNG(seed);
-test('RNG Float', function ()
+
+//----------- FLOAT --------\\
+test('RNG Float 1', function ()
 {
-    expect(RNG_1.nextFloat() === RNG_2.nextFloat());
-    expect(RNG_1.nextFloat() === RNG_2.nextFloat());
     expect(RNG_1.nextFloat() === RNG_2.nextFloat());
 });
 
-test('RNG Int', function ()
+test('RNG Float 2', function ()
 {
-    expect(RNG_1.nextInt() === RNG_2.nextInt());
-    expect(RNG_1.nextInt() === RNG_2.nextInt());
+    expect(RNG_1.nextFloat() === RNG_2.nextFloat());
+});
+
+test('RNG Float 3', function ()
+{
+    expect(RNG_1.nextFloat() === RNG_2.nextFloat());
+});
+
+test('RNG Float 4', function ()
+{
+    expect(RNG_1.nextFloat() === RNG_2.nextFloat());
+});
+
+//-------------- INT -----------\\
+test('RNG Int 1', function ()
+{
     expect(RNG_1.nextInt() === RNG_2.nextInt());
 });
 
-test('RNG Int', function ()
+test('RNG Int 2', function ()
 {
-    expect(RNG_1.nextInt() === RNG_2.nextInt());
-    expect(RNG_1.nextInt() === RNG_2.nextInt());
     expect(RNG_1.nextInt() === RNG_2.nextInt());
 });
 
-test('RNG Range', function ()
+test('RNG Int 3', function ()
+{
+    expect(RNG_1.nextInt() === RNG_2.nextInt());
+});
+
+test('RNG Int 4', function ()
+{
+    expect(RNG_1.nextInt() === RNG_2.nextInt());
+});
+
+
+test('RNG Range 1', function ()
 {
     expect(RNG_1.nextRange(0,1000) === RNG_2.nextInt(0,1000));
-    expect(RNG_1.nextRange(0,1000) === RNG_2.nextInt(0,1000));
-    expect(RNG_1.nextRange(0,1000) === RNG_2.nextInt(0,1000));
 });
+
+test('RNG Range 2', function ()
+{
+    expect(RNG_1.nextRange(-1,0) === RNG_2.nextInt(-1,0));
+});
+
+
+test('RNG Range 3', function ()
+{
+    expect(RNG_1.nextRange(-1000,1000) === RNG_2.nextInt(-1000,1000));
+});
+
+
+test('RNG Range 4', function ()
+{
+    expect(RNG_1.nextRange(0,0) === RNG_2.nextInt(0,0));
+});
+
 
--- Artefact/Tests/Vector2.test.js
+++ Artefact/Tests/Vector2.test.js
@@ -1,36 +1,75 @@
 let vec = require('../MainProject/lib/vector2');
 
-test('Vector Addition', function ()
+test('Vector Addition (0,0) + (1,1)', function ()
 {
     expect(vec.Vector2.Add(new vec.Vector2(0,0), new vec.Vector2(1,1))).toEqual(new vec.Vector2(1,1));
-    expect(vec.Vector2.Add(new vec.Vector2(-1,-1), new vec.Vector2(0,0))).toEqual(new vec.Vector2(-1,-1));
+});
 
+test('Vector Addition (-1,-1) + (0,0)', function ()
+{
+    expect(vec.Vector2.Add(new vec.Vector2(-1,-1), new vec.Vector2(0,0))).toEqual(new vec.Vector2(-1,-1));
+});
+
+test('Vector Addition Rand 1', function ()
+{
     let rand = Math.floor(Math.random()*10000);
     expect(vec.Vector2.Add(new vec.Vector2(rand,rand), new vec.Vector2(rand,0))).toEqual(new vec.Vector2(rand*2,rand));
-
-    rand = Math.floor(Math.random()*10000);
-    expect(vec.Vector2.Add(new vec.Vector2(rand,rand), new vec.Vector2(-rand,-rand))).toEqual(new vec.Vector2(0,0));
 });
-test('Vector Subtraction', function ()
+
+test('Vector Addition Rand 2', function ()
+{
+    let rand = Math.floor(Math.random()*10000);
+    expect(vec.Vector2.Add(new vec.Vector2(rand,rand), new vec.Vector2(rand,0))).toEqual(new vec.Vector2(rand*2,rand));
+});
+
+
+
+//--------------------- SUBTRACTION -------------------\\
+test('Vector Subtraction (0,0) - (1,1)', function ()
 {
     expect(vec.Vector2.Subtract(new vec.Vector2(0,0), new vec.Vector2(1,1))).toEqual(new vec.Vector2(-1,-1));
-    expect(vec.Vector2.Subtract(new vec.Vector2(-1,-1), new vec.Vector2(-1,-1))).toEqual(new vec.Vector2(0,0));
+});
 
+test('Vector Subtraction (-1,-1) - (-1,-1)', function ()
+{
+    expect(vec.Vector2.Subtract(new vec.Vector2(-1,-1), new vec.Vector2(-1,-1))).toEqual(new vec.Vector2(0,0));
+});
+
+test('Vector Subtraction Rand 1', function ()
+{
     let rand = Math.floor(Math.random()*10000);
     expect(vec.Vector2.Subtract(new vec.Vector2(rand,rand), new vec.Vector2(rand,0))).toEqual(new vec.Vector2(0,rand));
 
-    rand = Math.floor(Math.random()*10000);
+});
+
+test('Vector Subtraction Rand 2', function ()
+{
+    let rand = Math.floor(Math.random()*10000);
     expect(vec.Vector2.Subtract(new vec.Vector2(rand,rand), new vec.Vector2(-rand,-rand))).toEqual(new vec.Vector2(rand*2,rand*2));
 });
-test('Vector Scale', function ()
+
+
+//-------------------- SCALE ---------------\\
+test('Vector Scale (0,0) * 10', function ()
 {
     expect(vec.Vector2.Scale(new vec.Vector2(0,0),10)).toEqual(new vec.Vector2(0,0));
+});
+
+test('Vector Scale (-1,-1) - 10', function ()
+{
     expect(vec.Vector2.Scale(new vec.Vector2(-1,-1),-10)).toEqual(new vec.Vector2(10,10));
 
+});
+
+test('Vector Scale Rand 1', function ()
+{
     let rand = Math.floor(Math.random()*10000);
     expect(vec.Vector2.Scale(new vec.Vector2(rand,rand), rand)).toEqual(new vec.Vector2(rand*rand,rand*rand));
+});
 
-    rand = Math.floor(Math.random()*10000);
+test('Vector Scale Rand 2', function ()
+{
+    let rand = Math.floor(Math.random()*10000);
     expect(vec.Vector2.Scale(new vec.Vector2(rand,rand),-rand)).toEqual(new vec.Vector2(rand*rand*-1,rand*rand*-1));
 });
 
